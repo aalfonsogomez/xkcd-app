a4b7ba962513f37f7e1456e016adc5cdc694c298
Angel <a.alfonso.gomez@gmail.com>
2022-11-17 11:23:39 +0100

    Añadimos paginación a la página de comic

--- pages/comic/[id].js
+++ pages/comic/[id].js
@@ -2,9 +2,10 @@ import Head from 'next/head'
 import Link from 'next/link'
 import Image from 'next/image'
 import { Header } from 'components/Header'
-import { readFile } from 'fs/promises'
+import { stat, readdir, readFile } from 'fs/promises'
+import { basename } from 'path'
 
-export default function Comic({ id, img, alt, title, width, height }) {
+export default function Comic({ id, img, alt, title, width, height, nextId, prevId, hasNext, hasPrevious }) {
     return <>
         <Head>
             <title>xkcd - Comics for developers</title>
@@ -14,24 +15,40 @@ export default function Comic({ id, img, alt, title, width, height }) {
         <Header />
         <main>
             <section className="max-w-lg m-auto">
-                <h1 className="font-bold">{title}</h1>
-                <Image
-                    width={width}
-                    height={height}
-                    src={img}
-                    alt={alt}
-                />
+                <h1 className="font-bold text-xl text-center">{title}</h1>
+                <div className="max-w-xs m-auto mb-4">
+                    <Image
+                        layout='responsive'
+                        width={width}
+                        height={height}
+                        src={img}
+                        alt={alt}
+                    />
+                </div>
                 <p>{alt}</p>
+                <div class="flex justify-between mt-4 font-bold">
+                    {hasPrevious && <Link href={`/comic/${prevId}`}>
+                        <span className="text-blue-500">⬅ Previous</span>
+                    </Link>}
+                    {hasNext && <Link href={`/comic/${nextId}`}>
+                        <span className="text-blue-500">Next ➡</span>
+                    </Link>}
+                </div>
             </section>
         </main>
     </>
 }
 
 export async function getStaticPaths() {
+    const files = await readdir('comics')
+
+    const paths = files.map((file) => {
+        const id = basename(file, '.json')
+        return { params: { id } }
+    })
+
     return {
-        paths: [
-            { params: { id: '2597' } },
-        ],
+        paths,
         fallback: false
     }
 
@@ -39,11 +56,29 @@ export async function getStaticPaths() {
 
 export async function getStaticProps({ params }) {
     const { id } = params
+
     const content = await readFile(`comics/${id}.json`, 'utf8')
     const comic = JSON.parse(content)
+
+    const idNumber = +id
+    const prevId = idNumber - 1
+    const nextId = idNumber + 1
+
+    const [prevResult, nextResult] = await Promise.allSettled([
+        stat(`./comics/${prevId}.json`),
+        stat(`./comics/${nextId}.json`)
+    ])
+
+    const hasPrevious = prevResult.status === 'fulfilled'
+    const hasNext = nextResult.status === 'fulfilled'
+
     return {
         props: {
-            ...comic
+            ...comic,
+            hasPrevious,
+            hasNext,
+            prevId,
+            nextId
         }
     }
 }
