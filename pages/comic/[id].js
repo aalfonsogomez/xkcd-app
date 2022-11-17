46addf98cd6f43d06ccbe146f5efde75d3e805b4
Angel <a.alfonso.gomez@gmail.com>
2022-11-17 10:25:37 +0100

    Codificamos página con un sólo comic

--- pages/comic/[id].js
+++ pages/comic/[id].js
@@ -1,3 +1,49 @@
-export default function Comic() {
-    return <>null<style jsx>{``}</style></>;
+import Head from 'next/head'
+import Link from 'next/link'
+import Image from 'next/image'
+import { Header } from 'components/Header'
+import { readFile } from 'fs/promises'
+
+export default function Comic({ id, img, alt, title, width, height }) {
+    return <>
+        <Head>
+            <title>xkcd - Comics for developers</title>
+            <meta name="description" content="Comics for developers" />
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Header />
+        <main>
+            <section className="max-w-lg m-auto">
+                <h1 className="font-bold">{title}</h1>
+                <Image
+                    width={width}
+                    height={height}
+                    src={img}
+                    alt={alt}
+                />
+                <p>{alt}</p>
+            </section>
+        </main>
+    </>
+}
+
+export async function getStaticPaths() {
+    return {
+        paths: [
+            { params: { id: '2597' } },
+        ],
+        fallback: false
+    }
+
+}
+
+export async function getStaticProps({ params }) {
+    const { id } = params
+    const content = await readFile(`comics/${id}.json`, 'utf8')
+    const comic = JSON.parse(content)
+    return {
+        props: {
+            ...comic
+        }
+    }
 }
